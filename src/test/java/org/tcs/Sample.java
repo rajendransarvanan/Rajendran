a8586eea72e97aa0673af1c2578c044fe83adbfc
rajendransaravanan <rajendransarvananofficial@gmail.com>
2022-05-27 21:59:35 +0530

    methodsadded

---
+++ src/test/java/org/tcs/Sample.java
@@ -0,0 +1,22 @@
+package org.tcs;
+
+public class Sample {
+	private void yokesh() {
+		System.out.println("yokesh");
+
+	}
+	
+	private void sasi() {
+System.out.println("sasi");
+	}
+	
+	private void vinoth() {
+System.out.println("vinoth");
+	}
+	private void raju() {
+System.out.println("raju");
+	}
+
+	
+	
+}
