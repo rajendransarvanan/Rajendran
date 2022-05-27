aa723bcd5d3f1f898a06fd6a3a95884591b55eac
rajendransaravanan <rajendransarvananofficial@gmail.com>
2022-05-27 12:29:12 +0530

    first commit

---
+++ src/org/lang/LanguageInfo.java
@@ -0,0 +1,22 @@
+package org.lang;
+
+public class LanguageInfo {
+	private void tamillanguage() {
+		System.out.println("Tamil language is oldest living language in the World");
+	}
+	private void englighlanguage() {
+		System.out.println("English language is said to be happiest language in the World");
+	}
+	private void hindilanguage() {
+		System.out.println("Hindi language is preferred as official language in India");
+	}
+	public static void main(String[] args) {
+		LanguageInfo l= new LanguageInfo();
+		l.tamillanguage();
+		l.englighlanguage();
+		l.hindilanguage();
+		
+	}
+	
+
+}
